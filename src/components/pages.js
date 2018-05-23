adf7a178efeb603a5fcc35e8eeb903ff093c3997
Shawn Sandy <shawnsandy04@gmail.com>
2018-05-23 16:13:37 -0400

    chamge import  to require

--- src/components/pages.js
+++ src/components/pages.js
@@ -1,7 +1,7 @@
 import riot from 'riot'
 
-import "../../node_modules/blacktie-pages/page.tag"
-// import 'blacktie-pages/page.tag'
+require ("blacktie-pages/page.tag")
+//import 'blacktie-pages/page.tag'
 import './pages/landing.tag'
 import './pages/components.tag'
 
