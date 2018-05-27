b4f1791e4646fa02b0e56694af8a55d18c714cc3
Shawn Sandy <shawnsandy04@gmail.com>
2018-05-27 05:35:52 -0400

    packages - add babel loader/core

--- packages/blacktie-pages/index.js
+++ packages/blacktie-pages/index.js
@@ -3,3 +3,6 @@ import route from 'riot-route'
 
 import './src/page.tag'
 import './src/demo-page.tag'
+console.log("page component");
+
+riot.mount('*')
--- src/components/pages.js
+++ src/components/pages.js
@@ -2,7 +2,7 @@ import riot from 'riot'
 import "riot-hot-reload"
 import route from 'riot-route'
 // import 'blacktie-pages'
-require ("blacktie-pages/index.js")
+import "blacktie-pages/index.js"
 
 
 //import 'blacktie-pages/page.tag'
