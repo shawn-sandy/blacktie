4dd0a60f1f13a4254c6f44f4357ff9a1815b4c58
Shawn Sandy <shawnsandy04@gmail.com>
2018-05-27 06:33:20 -0400

    add function to captoure and process node.argv arguments

--- gulpfile.js
+++ gulpfile.js
@@ -3,23 +3,23 @@ const sass = require("gulp-sass");
 const print = require("gulp-print");
 const cssnano = require("gulp-cssnano");
 const reports = require("gulp-sizereport");
+const copy = require('gulp-copy')
 
-gulp.task("sass", () =>
-  gulp
-    .src("./src/stylesheets/*.scss", { base: "./src/stylesheets" })
-    .pipe(sass().on("error", sass.logError))
-    .pipe(reports({ gzip: true }))
-    .pipe(cssnano())
-    .pipe(gulp.dest("./gist/css"))
-    .pipe(reports({ gzip: true }))
-);
+// fetch command line arguments
+const arg = (argList => {
 
-gulp.task("watch:sass", () => gulp.watch("./src/css/**/*.scss", ["sass"]));
+  let arg = {}, a, opt, thisOpt, curOpt;
+  for (a = 0; a < argList.length; a++) {
 
-gulp.task("reports", () =>
-  gulp.src("./dist/**/*.*")
-  .pipe(reports({ gzip: true }))
-);
+    thisOpt = argList[a].trim();
+    opt = thisOpt.replace(/^\-+/, '');
+
+    if (opt === thisOpt) {
+
+      // argument value
+      if (curOpt) arg[curOpt] = opt;
+      curOpt = null;
+
     }
     else {
 
