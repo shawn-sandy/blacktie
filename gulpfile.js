a1f4f6eec6ef7c41cfa6dd6a07088ed156c5f8f9
Shawn Sandy <shawnsandy04@gmail.com>
2018-05-27 06:32:06 -0400

    add gulp-task new package

--- gulpfile.js
+++ gulpfile.js
@@ -20,3 +20,39 @@ gulp.task("reports", () =>
   gulp.src("./dist/**/*.*")
   .pipe(reports({ gzip: true }))
 );
+    }
+    else {
+
+      // argument name
+      curOpt = opt;
+      arg[curOpt] = true;
+
+    }
+
+  }
+
+  return arg;
+
+})(process.argv);
+
+gulp.task("sass", () =>
+  gulp
+    .src("./src/stylesheets/*.scss", { base: "./src/stylesheets" })
+    .pipe(sass().on("error", sass.logError))
+    .pipe(reports({ gzip: true }))
+    .pipe(cssnano())
+    .pipe(gulp.dest("./gist/css"))
+    .pipe(reports({ gzip: true }))
+);
+
+gulp.task("watch:sass", () => gulp.watch("./src/css/**/*.scss", ["sass"]));
+
+gulp.task("reports", () =>
+  gulp.src("./dist/**/*.*")
+  .pipe(reports({ gzip: true }))
+);
+
+gulp.task("new-package", () => {
+  let package_name = arg.name || arg.n
+  console.log(package_name);
+})
