c960d6cf6807e1a92acd3e504e6c35038e412d88
Shawn Sandy <shawnsandy04@gmail.com>
2018-06-07 14:02:17 -0400

    WIP - sass loader
    - add micro-sass-loader to packages
    -

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,72 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path")
 
 module.exports = {
 	entry: {
 		app: __dirname + "/src/app.js",
 		components: __dirname + "/src/components/index.js",
-	  pages: __dirname + "/src/components/pages.js"
+		pages: __dirname + "/src/components/pages.js"
 	},
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-	  },
-	  {
-        test: /\.vue$/,
-        loader: "vue-loader",
-        options: {}
-      },
-      {
-        test: /\.tag$/,
-        exclude: /node_modules/,
-        loader: "riot-tag-loader"
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader",
-            options: { minimize: true }
-          }
-        ]
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"]
-	  },
-	  {
-		test: /\.scss$/,
-		use: ["style-loader", "css-loader", "sass-loader"],
-	  }
-    ]
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html"
-    }),
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css"
-    })
-  ]
+	module: {
+		rules: [{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader"
+				}
+			},
+			{
+				test: /\.vue$/,
+				loader: "vue-loader",
+				options: {}
+			},
+			{
+				test: /\.tag$/,
+				exclude: /node_modules/,
+				loader: "riot-tag-loader"
+			},
+			{
+				test: /\.html$/,
+				use: [{
+					loader: "html-loader",
+					options: {
+						minimize: true
+					}
+				}]
+			},
+			{
+				test: /\.css$/,
+				use: [MiniCssExtractPlugin.loader, "css-loader"]
+			},
+			{
+				test: /\.scss$/,
+				use: [{
+						loader: "micro-style-loader",
+						options: {
+							sourceMaps: true
+						}
+					},
+					{
+						loader: "css-loader",
+						options: {}
+					},
+					{
+						loader: "sass-loader",
+						options: {}
+					}
+				]
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebPackPlugin({
+			template: "./src/index.html",
+			filename: "./index.html"
+		}),
+		new MiniCssExtractPlugin({
+			filename: "[name].css",
+			chunkFilename: "[id].css"
+		})
+	]
 };
