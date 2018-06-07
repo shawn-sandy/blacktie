559b71d262f64ab0feec875e637a11ad118217dd
Shawn Sandy <shawnsandy04@gmail.com>
2018-06-07 18:04:34 -0400

    WIP - component updates

--- src/components/index.js
+++ src/components/index.js
@@ -17,4 +17,10 @@ import 'blacktie-icons/bt-icon-material.tag'
 import 'blacktie-loaders/spin-loader/bt-loader-spin.tag'
 import 'blacktie-loaders/processing/bt-loader-processing.tag'
 
+import "blacktie-pages/index.js"
+
+//import 'blacktie-pages/page.tag'
+import './pages/landing.tag'
+import './pages/components.tag'
+
 riot.mount('*')
