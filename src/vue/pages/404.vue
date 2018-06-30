423861f6ecb9200326bd50a41e23adc3f1198ab9
Shawn Sandy <shawnsandy04@gmail.com>
2018-06-29 20:41:06 -0400

    404 page vue

--- src/vue/pages/404.vue
+++ src/vue/pages/404.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
     <page-component>
-    <div class="missing">
-      <div class="flex-box flex-centered text-center">
-        <div class="wrapper">
-          <p class="display-4 text-danger">
-            404 Error! You broke it :(
-          </p>
-          <p class="text-danger">Just kidding, we will have someone look into this ASAP</p>
+      <div class="missing">
+        <div class="flex-box flex-centered text-center">
+          <div class="wrapper">
+            <p class="display-4 text-danger">
+              404 Error! You broke it :(
+            </p>
+            <p class="text-danger">Just kidding, we will have someone look into this ASAP</p>
+          </div>
         </div>
       </div>
-    </div>
     </page-component>
   </div>
 </template>
