24e3224d7ba639b0677096f698d1f0cf16b8f668
Shawn Sandy <shawnsandy04@gmail.com>
2018-07-04 20:14:31 -0400

    Display 404 page if there is a pg parameter query in the url string

--- src/router.js
+++ src/router.js
@@ -4,32 +4,41 @@ console.log('path name', window.location.pathname)
 const Home = require('./vue/pages/index.vue').default
 const NotFound = require('./vue/pages/404.vue').default
 
-const routes  = {
+const routes = {
   '/': Home
 }
 
 const getQueryString = function (field, url) {
-	var href = url ? url : window.location.href;
-	var reg = new RegExp('[?&]' + field + '=([^&#]*)', 'i');
-	var string = reg.exec(href);
-	return string ? string[1] : null;
+  var href = url ? url : window.location.href;
+  var reg = new RegExp('[?&]' + field + '=([^&#]*)', 'i');
+  var string = reg.exec(href);
+  return string ? string[1] : null;
 }
 
-const page =  getQueryString('pg');
+const page = getQueryString('pg');
 console.log('page', page);
 
 new Vue({
   el: '#routes',
+  custom: "custom option",
   data() {
     return {
       currentRoute: window.location.pathname,
     }
   },
   computed: {
-    ViewComponent () {
-     console.log('pg', page)
-      return routes[page] || Home
+    ViewComponent() {
+
+      console.log('pg', page)
+      console.log('option', this.$options.custom)
+
+      if(page)
+      return routes[page] || NotFound
+
+      return Home
     }
   },
-  render (h) { return h(this.ViewComponent) }
+  render(h) {
+    return h(this.ViewComponent)
+  }
 })
