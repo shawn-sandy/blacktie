1523641e0b0fe678a804e7b7b3964567e485704b
Shawn Sandy <shawnsandy04@gmail.com>
2018-07-25 21:31:01 -0400

    update vue config

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,6 +1,6 @@
 module.exports = {
  components: 'packages/**/src/components/*.vue',
- styleguideDir: './dist/docs/vue/index.html',
+ styleguideDir: './dist/docs/vue/',
  showUsage: true,
  title: 'BlackTie Vue Components'
 
