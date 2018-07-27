e1ca9161de53603d41fb40608da41293b2a70a4b
Shawn Sandy <shawnsandy04@gmail.com>
2018-07-27 08:21:02 -0400

    Integrate setup configs

--- packages/blacktie-loading-dots/styleguide.config.js
+++ packages/blacktie-loading-dots/styleguide.config.js
@@ -1,7 +1,6 @@
 module.exports = {
-  components: 'packages/**/src/components/*.vue',
-  styleguideDir: './dist/docs/vue/',
-  showUsage: true,
-  title: 'BlackTie Vue Components'
-
+  components: 'src/components/**/*.vue',
+  showUsage: false,
+  showCode: true,
+  title: 'Loading Dots'
 }
