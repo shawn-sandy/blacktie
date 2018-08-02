6f79787ca190e69893d8cd9c4f5f394bc1228dba
Shawn Sandy <shawnsandy04@gmail.com>
2018-08-02 10:15:38 -0400

    Scroll bar base component

--- packages/blacktie-scrollbar/src/App.vue
+++ packages/blacktie-scrollbar/src/App.vue
@@ -1,30 +1,45 @@
 <template>
   <div id="app" class="flex-elm">
-    <HelloWorld msg="Blacktie Scrollbar demo"/>
+    <div class="container">
+      <bt-scrollbar>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa dolor tenetur quae iste odio maiores laborum quisquam autem dolorem necessitatibus. Expedita aspernatur fugit incidunt? Molestias voluptates nisi nihil sint deserunt.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa dolor tenetur quae iste odio maiores laborum quisquam autem dolorem necessitatibus. Expedita aspernatur fugit incidunt? Molestias voluptates nisi nihil sint deserunt.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa dolor tenetur quae iste odio maiores laborum quisquam autem dolorem necessitatibus. Expedita aspernatur fugit incidunt? Molestias voluptates nisi nihil sint deserunt.</p>
+      </bt-scrollbar>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/Index.vue'
+import BtScrollbar from './components/Scrollbar.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    BtScrollbar
   }
 }
 </script>
 
 <style lang="scss">
-@import "~blacktie-styles/app.scss";
+@import '~blacktie-styles/app.scss';
 
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   height: 100vh;
 
+  .container {
+   width: 300px;
+   height: 250px;
+   margin-left: auto;
+   margin-right: auto;
+   overflow: hidden;
+  }
 }
+@import './assets/variables.scss';
+@import './assets/style.scss';
 </style>
---
+++ packages/blacktie-scrollbar/src/components/Scrollbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="bt-simple-bar" :class=barClass data-simplebar data-simplebar-auto-hide="true">
+    <div className="simplebar-scroll-content" style="">
+      <div className="simplebar-content">
+        <!-- @slot add content inside scrollbar -->
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ScrollBar from 'simplebar'
+
+export default {
+  name: 'BlacktieScrollbar',
+  props: {
+    /**
+     *
+     */
+    barClass: {
+      type: String,
+      default: 'bt-simple-bar-wrapper'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import "~simplebar/dist/simplebar.css";
+</style>
