0b9bf51f0d847189eb10cf4f8dafd7492284daa3
Shawn Sandy <shawnsandy04@gmail.com>
2018-08-05 23:27:19 -0400

    WIP theme config

---
+++ styleguide.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+module.exports = {
+  components: 'packages/**/src/components/*.vue',
+  styleguideDir: './dist/docs/vue/',
+  showUsage: true,
+  showCode: true,
+  title: 'BlackTie Components',
+  navigation: true,
+  skipComponentsWithoutExample: true,
+  serverPort: 6069,
+  theme: {
+    maxWidth: '80%',
+    sidebarWidth: 250,
+    color: {
+        link: '#FFF',
+        linkHover: 'rgb(70, 69, 171)',
+        sidebarBackground: '#333333',
+        errorBackground: '#e22d44',
+        name: '#FFF'
+    }
+},
+styles: {
+  Logo: {
+    logo: {
+        color: '#fff',
+        fontSize: 20
+    }
+}
+}
+};
