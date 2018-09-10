a5ba613165e9763acdaf209bd28df9fbe838e9a6
Shawn Sandy <shawnsandy04@gmail.com>
2018-09-10 01:13:58 -0400

    Move scss code to external file

--- packages/blacktie-buttons/src/components/AppButton.vue
+++ packages/blacktie-buttons/src/components/AppButton.vue
@@ -43,58 +43,5 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-@import 'sass-rem';
-button, .button-elm {
-  display: inline-block;
-  border: none;
-  padding: rem(14px 28px);
-  margin: 0;
-  text-decoration: none;
-  background: gray;
-  color: #ffffff;
-  font-size: rem(14px);
-  font-style: normal;
-  font-weight: 200;
-  letter-spacing: rem(.5px);
-  cursor: pointer;
-  text-align: center;
-  transition: background 250ms ease-in-out, transform 150ms ease;
-  -webkit-appearance: none;
-  -moz-appearance: none;
-  border-radius: rem(2px);
-}
-
-button:hover,
-button:focus,
-.button-elm:hover,
-.button-elm:focus
- {
-  background: darken($color: gray, $amount: 10);
-  transform: scale(0.99);
-}
-
-button:focus,
-.button-elm:focus
- {
-    outline: none;
-    background: darken($color: gray, $amount: 12);
-}
-
-
-button:active,
-.button-elm:active
- {
-  transform: scale(0.98);
-}
-
-button.rounded, .button-elm.rounded {
-
-  border-radius: rem(100px);
-
-}
-
-button.button-lg, .button-elm.button-lg {
-  font-size: rem(24px);
-   padding: rem(14px 48px);
-}
+@import './scss/app-button.scss'
 </style>
