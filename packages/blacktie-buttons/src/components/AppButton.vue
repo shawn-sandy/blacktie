808d40cdb7cfd85c54eb504f91fc80a5c1d125fa
Shawn Sandy <shawnsandy04@gmail.com>
2018-09-09 22:27:16 -0400

    WIP - Button

--- packages/blacktie-buttons/src/components/AppButton.vue
+++ packages/blacktie-buttons/src/components/AppButton.vue
@@ -1,6 +1,6 @@
    <template>
   <button
-    class="button color is-large"
+    class=""
     @click="handleClick">
     <!-- @slot Use this slot to place the button content -->
     <slot>Button</slot>
@@ -8,11 +8,11 @@
 </template>
 
 <script>
-import sizeMixin from './sizeMixin';
+
 
 export default {
   name: 'AppButton',
-  mixins: [sizeMixin],
+
   props: {
     /**
      * Sets the button font color
@@ -56,7 +56,35 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-.color {
-  color: darken($color: blue, $amount: 10%);
+button {
+    display: inline-block;
+    border: none;
+    padding: 1rem 2rem;
+    margin: 0;
+    text-decoration: none;
+    background: #0069ed;
+    color: #ffffff;
+    font-family: sans-serif;
+    font-size: 1rem;
+    cursor: pointer;
+    text-align: center;
+    transition: background 250ms ease-in-out,
+                transform 150ms ease;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+}
+
+button:hover,
+button:focus {
+    background: #0053ba;
+}
+
+button:focus {
+    outline: 1px solid #fff;
+    outline-offset: -4px;
+}
+
+button:active {
+    transform: scale(0.99);
 }
 </style>
