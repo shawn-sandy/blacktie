cc8935cd395fb3d4d146003f599fb0b0db896075
Shawn Sandy <shawnsandy04@gmail.com>
2018-09-14 04:40:49 -0400

    - Add @vue/eslint-config-prettier - code cleanup

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,33 +1,31 @@
-const path = require("path")
+const path = require('path')
 module.exports = {
-  components: "packages/**/src/components/*.vue",
-  styleguideDir: "./dist/guide/",
+  components: 'packages/**/src/components/*.vue',
+  styleguideDir: './dist/guide/',
   usageMode: 'expand',
-  title: "BlackTie Components",
+  title: 'BlackTie Components',
   pagePerSection: true,
   skipComponentsWithoutExample: true,
   serverPort: 6069,
   theme: {
-    maxWidth: "80%",
+    maxWidth: '80%',
     sidebarWidth: 350,
     color: {
-      link: "#79838A",
-      linkHover: "rgb(70, 69, 171)",
-      sidebarBackground: "#FFFFFF",
-      border: "#51677A",
-      errorBackground: "#e22d44",
-      name: "#2B3338"
+      link: '#79838A',
+      linkHover: 'rgb(70, 69, 171)',
+      sidebarBackground: '#FFFFFF',
+      border: '#D2D3D7',
+      errorBackground: '#e22d44',
+      name: '#2B3338'
     }
   },
-  require: [
-    './src/javascripts/polyfill.min.js'
-  ],
+  require: [path.resolve('./src/javascripts/polyfill.min.js')],
   styles: {
     Logo: {
       logo: {
-        color: "#2B3338",
+        color: '#2B3338',
         fontSize: 20
       }
     }
   }
-};
+}
