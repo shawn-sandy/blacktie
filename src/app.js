0bfc25a0504bf2b9feb8097916b959dbafa1b0a6
Shawn Sandy <shawnsandy04@gmail.com>
2018-09-20 23:49:21 -0400

    WIP - mixin modifier

--- src/app.js
+++ src/app.js
@@ -1,51 +1,49 @@
-import "blacktie-styles/app.scss"
-import "./css/styles.scss"
-
+import 'blacktie-styles/app.scss'
+import './css/styles.scss'
 
 // let test = new TestClass();
-import { u } from "umbrellajs";
+import { u } from 'umbrellajs'
 
-const validate = require("validate");
-const smoothScroll = require("smooth-scroll");
-
+const validate = require('validate')
+const smoothScroll = require('smooth-scroll')
 
 /** simple and easy form validation */
-validate.init();
+validate.init()
 
 /** scroll to anchors */
-const scroll = new smoothScroll('a[href*="#"]');
+const scroll = new smoothScroll('a[href*="#"]')
 
 /**
  * Confirm actions before submit
  * Prompt user before deleting data
  */
-u(".confirm-actions").each((elm, i) => {
+u('.confirm-actions').each((elm, i) => {
   if (elm) {
     // grab the default element html value
-    const val = u(elm).html();
+    const val = u(elm).html()
     //console.log(val);
 
     // listen for the click event
-    u(elm).on("click", e => {
+    u(elm).on('click', e => {
       // set the confirmation value
       // search for data-confirm-html attribute
       // or defaults to confirm
-      const confirmation = u(elm).data("confirm-html") || "Confirm";
-      const timer = u(elm).data("timeout") || 4000;
+      const confirmation = u(elm).data('confirm-html') || 'Confirm'
+      const timer = u(elm).data('timeout') || 4000
 
       // check if element html value is the same as the confirmation
       // if is reset to original value else set to confirmation
       // sets a 5 second timeout for user to respond
       // or reset the element to original value
       if (confirmation === u(elm).html()) {
-        u(elm).html(val);
+        u(elm).html(val)
       } else {
-        u(elm).html(confirmation);
+        u(elm).html(confirmation)
         setTimeout(() => {
-          u(elm).html(val);
-        }, timer);
-        e.preventDefault();
+          u(elm).html(val)
+        }, timer)
+        e.preventDefault()
       }
-    });
+    })
   }
-});
+})
