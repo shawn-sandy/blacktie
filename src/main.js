c4ca0565e92f338cf2e8839c406d0c07ae99a0c3
Shawn Sandy <shawnsandy04@gmail.com>
2018-10-07 13:02:05 -0400

    Updates the home page - remove demo components - (add components to build pages)

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
+// import Vue from 'vue'
+// import App from './App.vue'
 import './registerServiceWorker'
 import './app.js'
 
-Vue.config.productionTip = false
+// Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App)
-}).$mount('#app')
+// new Vue({
+//   render: h => h(App)
+// }).$mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -60,7 +60,20 @@ module.exports = {
     // template is inferred to be `public/subpage.html`
     // and falls back to `public/index.html` if not found.
     // Output filename is inferred to be `subpage.html`.
-    components: 'src/main.js'
+    components: {
+      // entry for the page
+      entry: './src/components.js',
+      // the source template
+      template: 'public/components.html',
+      // output as dist/index.html
+      filename: 'components.html',
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      title: 'Index Page',
+      // chunks to include on this page, by default includes
+      // extracted common chunks and vendor chunks.
+      chunks: ['chunk-vendors', 'chunk-common', 'components']
+    }
   },
   devServer: {
     overlay: {
