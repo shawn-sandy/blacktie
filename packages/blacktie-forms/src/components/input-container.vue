83dffddffe66c9816d842060f5fd14be4d10403d
Shawn Sandy <shawnsandy04@gmail.com>
2018-11-25 12:08:52 -0500

    Refactor the input wrapper -  remove calidate functions - update props - add computer function labelFor

--- packages/blacktie-forms/src/components/input-container.vue
+++ packages/blacktie-forms/src/components/input-container.vue
@@ -1,44 +1,36 @@
 <template>
   <div class="input-container">
-    <div
-      ref="field-wrapper"
-      class="input-elm-wrapper">
-      <slot
-        :errorNotify="errorNotify"
-        :removeError="removePopper"/>
-    </div>
-    <slot name="error-elm">
-      <transition name="fade">
-        <div
-          v-show="showErrors"
-          ref="inErrors"
-          class="validation-msg">
-          {{ errorMsg }}
-        </div>
-      </transition>
-    </slot>
+    <label
+      :lableClass="labelClass"
+      for="labelFor(label)" />
+    <slot />
   </div>
 </template>
-
 
 <script>
 /**
  * BlackTie input container with baked pop-up error notifications powered by popper.js
  */
-import containerMixin from './../mixins/input-container.js'
+
 export default {
   name: 'InputContainer',
-  mixins: [containerMixin]
+  props: {
+    label: {
+      type: String,
+      default: 'Label'
+    },
+    labelClass: {
+      type: String,
+      default: 'label'
+    }
+  },
+  computed: {
+    lableFor(name) {
+      if (!name) return
+      return name.replace(' ', '_').toLowerCase
+    }
+  }
 }
 </script>
 <style lang="scss">
-/* Enter and leave animations can use different */
-/* durations and timing functions.*/
-.fade-enter-active {
-  transition: opacity 0.5s;
-}
-/* .fade-leave-active below version 2.1.8 */
-.fade-enter {
-  opacity: 0;
-}
 </style>
