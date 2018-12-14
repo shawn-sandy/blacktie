3a0640799db9809141b9a02fbaa065e32fcc5c45
Shawn Sandy <shawnsandy04@gmail.com>
2018-12-14 06:58:47 -0500

    Lint cleanup

--- src/App.vue
+++ src/App.vue
@@ -3,37 +3,43 @@
     <!-- <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App..."/> -->
     <div>
-      <cover-img>
-        <h1 class="title is-1 blacktie-smoke">Cover Title</h1>
-      </cover-img>
+      <CoverImg>
+        <h1 class="title is-1 blacktie-smoke">
+          Cover Title
+        </h1>
+      </CoverImg>
     </div>
     <p>
-      <button-element
+      <ButtonElement
         class="blacktie-primary-elm"
-        display-confirmation >
+        display-confirmation
+      >
         <template slot="confirmation">
           <span>&#10095;</span>
           Please Confirm
         </template>
         <span>&#10095;</span>
         <span>Confirmation Buttons</span>
-      </button-element>
+      </ButtonElement>
     </p>
     <p>
-      <url-button
+      <UrlButton
         href="//getblacktie.com"
         class="button-elm-lg elm-rounded"
-        display-confirmation>
-        <template slot="confirmation"><span>&#10095;</span>Please Confirm</template>
+        display-confirmation
+      >
+        <template slot="confirmation">
+          <span>&#10095;</span>Please Confirm
+        </template>
         <span>&#10095;</span>
         <span>Confirmation Button</span>
-      </url-button>
+      </UrlButton>
     </p>
     <hr>
     <form @submit.prevent="">
       <div class="field">
         <div class="control">
-          <input-component
+          <InputComponent
             id="myfield"
             v-model="email"
             elm-class="input"
@@ -43,11 +49,12 @@
             placeholder="Enter your Em@il address"
             required
             error-message="Please enter a valid email address"
-            @error-msg="alert"/>
+            @error-msg="alert"
+          />
         </div>
       </div>
 
-      <input-component
+      <InputComponent
         id="myfielsz-"
         v-model="numbers"
         elm-class="input"
@@ -57,18 +64,20 @@
         minlength="5"
         required
         error-message="Please enter your full name"
-        @error-msg="alert"/>
+        @error-msg="alert"
+      />
 
 
       <p class="has-text-right">
         <button
           class="button"
-          formnovalidate>
+          formnovalidate
+        >
           Submit
         </button>
       </p>
       <!-- </input-elm> -->
-      <div/>
+      <div />
     </form>
   </div>
 </template>
@@ -79,14 +88,14 @@ import ButtonElement from 'blacktie-buttons-v2/src/components/ButtonElm.vue'
 import UrlButton from 'blacktie-buttons-v2/src/components/UrlButtonElm.vue'
 import InputComponent from 'blacktie-forms/src/components/input-elm.vue'
 // import InputElm from 'blacktie-forms/src/components/input-container.vue'
-import ValidationForm from 'blacktie-forms/src/components/FormElement.vue'
+// import ValidationForm from 'blacktie-forms/src/components/FormElement.vue'
 export default {
   name: 'App',
   components: {
     ButtonElement,
     UrlButton,
     InputComponent,
-    ValidationForm,
+    // ValidationForm,
 
     CoverImg: () => import('blacktie-img/src/components/CoverImg.vue')
   },
